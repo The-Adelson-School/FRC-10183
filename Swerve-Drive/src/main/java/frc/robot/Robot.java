473c2b1e85f2257c342a237bec32f187e773ced5
arip613 <ari.polonsky@adelsoncampus.org>
2025-02-27 15:31:03 -0800

    Update Robot.java

--- Swerve-Drive/src/main/java/frc/robot/Robot.java
+++ Swerve-Drive/src/main/java/frc/robot/Robot.java
@@ -54,3 +54,36 @@ public class Robot extends TimedRobot {
     m_swerve.drive(xSpeed, ySpeed, rot, fieldRelative, getPeriod());
   }
 }
+
+public class Robot extends TimedRobot {
+
+    private RobotContainer robotContainer;
+    private XboxController controller;
+    private ElevatorSubsystem elevator;
+
+    @Override
+    public void robotInit() {
+        robotContainer = new RobotContainer();
+        controller = new XboxController(0); // Initialize the controller
+        elevator = robotContainer.getElevatorSubsystem(); //get elevator subsystem.
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        CommandScheduler.getInstance().run();
+
+  
+        if (controller.getXButtonPressed()) {
+            elevator.setStage(elevator.getStage1Height());
+        } else if (controller.getYButtonPressed()) {
+            elevator.setStage(elevator.getStage2Height());
+        } else if (controller.getAButtonPressed()) {
+            elevator.setStage(elevator.getStage3Height());
+        }
+    }
+
+    @Override
+    public void robotPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
+}
