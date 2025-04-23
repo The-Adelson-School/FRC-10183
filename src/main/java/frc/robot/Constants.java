86725bfd837e3ce724e7efceb9c84042e6bd49c7
FRC 10183 <arc@adelsoncampus.org>
2025-04-23 15:58:04 -0700

    initial commit from dell PC

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,77 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+import swervelib.math.Matter;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean constants. This
+ * class should not be used for any other purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants
+{
+
+  public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
+  public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
+  public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
+  public static final double MAX_SPEED  = Units.feetToMeters(18.5);
+  // Maximum speed of the robot in meters per second, used to limit acceleration.
+
+//  public static final class AutonConstants
+//  {
+//
+//    public static final PIDConstants TRANSLATION_PID = new PIDConstants(0.7, 0, 0);
+//    public static final PIDConstants ANGLE_PID       = new PIDConstants(0.4, 0, 0.01);
+//  }
+
+  public static final class DrivebaseConstants
+  {
+
+    // Hold time on motor brakes when disabled
+    public static final double WHEEL_LOCK_TIME = 10; // seconds
+  }
+
+  public static class OperatorConstants
+  {
+
+    // Joystick Deadband
+    public static final double DEADBAND        = 0.1;
+    public static final double LEFT_Y_DEADBAND = 0.1;
+    public static final double RIGHT_X_DEADBAND = 0.1;
+    public static final double TURN_CONSTANT    = 6;
+  }
+
+  public static class ElevatorConstants{
+    //Elevator Math
+    public static final int COUNTS_PER_ROTATION = 3402;
+    public static final int MAX_EXTENTION_INCHES = 11;
+    public static final int INCHES_PER_ROTATION = 4; //replace with actual when determined
+    public static final int COUNTS_PER_INCH = COUNTS_PER_ROTATION/INCHES_PER_ROTATION;
+    
+    
+    //Elevator Set Points
+    public static final int STOWED_LEVEL = 0;
+    public static final int CORAL_STATION = 0; // change when actual determined
+    public static final int LEVEL_ONE = 0; // change when actual determined
+    public static final int LEVEL_TWO = 0; // change when actual determined
+    public static final int LEVEL_THREE = 0; // change when actual determined
+
+    //Wrist Set Points
+    public static final int STOWED_ANGLE = 0; // change when actual determined
+    public static final int CORAL_ANGLE = 0; // change when actual determined
+    public static final int SCORING_ANGLE = 0; // change when actual determined
+
+    //Intake Speeds
+    public static final double INTAKE_IN = 1.0; //Maybe needs to be reversed
+    public static final double INTAKE_STOP = 0.0;
+    public static final double INTAKE_OUT = -1.0;
+  }
+}
